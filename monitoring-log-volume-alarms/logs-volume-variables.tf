f8a69dada2c8c88a1ebaf65cd5e3114be64bfd3d
Eugeniusz Neugebauer <eugeniusz.neugebauer@telia.no>
2021-05-31 10:12:27 +0200

    ANOMALY_DETECTION_BAND to 3

--- monitoring-log-volume-alarms/logs-volume-variables.tf
+++ monitoring-log-volume-alarms/logs-volume-variables.tf
@@ -11,6 +11,6 @@ variable "sns_topic_arn" {
 
 variable "env_description" {
   description = "Short environment description"
-  type = string
-  default = "()"
+  type        = string
+  default     = "()"
 }
