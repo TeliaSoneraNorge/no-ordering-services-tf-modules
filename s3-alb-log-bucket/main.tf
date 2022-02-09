7044485e4a75ccd5ea8d143b4a177969f6ba5a9f
Dalibor Lis <dalibor.lis@telia.no>
2022-02-09 08:34:50 +0100

    Fix creating ALB S3 bucket.

--- s3-alb-log-bucket/main.tf
+++ s3-alb-log-bucket/main.tf
@@ -51,7 +51,9 @@ resource "aws_s3_bucket" "logs" {
 }
 
 resource "aws_s3_bucket_public_access_block" "logs" {
-  bucket = aws_s3_bucket.logs.id
+  for_each = aws_s3_bucket.logs
+
+  bucket = each.value.id
 
   block_public_acls       = true
   block_public_policy     = true
