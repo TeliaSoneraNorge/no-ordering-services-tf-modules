c6411cbd84088f191d660afa81d6afa35b9e6a75
Eugeniusz Neugebauer <eugeniusz.neugebauer@telia.no>
2023-04-06 14:09:25 +0200

    ECS task def cleaner, TF state
    # Conflicts:
    #	ecs-task-definition-cleaner-lambda/main.tf
    #	ecs-task-definition-cleaner-lambda/variables.tf

--- ecs-task-definition-cleaner-lambda/variables.tf
+++ ecs-task-definition-cleaner-lambda/variables.tf
@@ -14,3 +14,17 @@ variable "old_revision_count" {
   type        = string
   default     = "10"
 }
+
+#implemented in relation to the bug https://github.com/hashicorp/terraform-provider-aws/issues/29749
+variable "s3_tf_state_files_for_checking_ref" {
+  description = "Terraform S3 state files URI used for checking existing references, pls separate files with comma"
+  type        = string
+  default     = ""
+}
+
+variable "s3_tf_state_buckets" {
+  description = "Terraform S3 state buckets (used by IAM)"
+  type        = list(string)
+  default     = []
+}
+
