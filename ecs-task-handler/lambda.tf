da7345c3e691a1d156eefe1ef440a37c07c9ec3b
Vladimir Jarotek <vladimir.jarotek@gmail.com>
2023-05-04 13:32:26 +0200

    ecs describe task permission added to allow ECS task failing lambda check status

--- ecs-task-handler/lambda.tf
+++ ecs-task-handler/lambda.tf
@@ -90,7 +90,10 @@ data "aws_iam_policy_document" "ecs_task_failing_handler_lambda" {
 
   statement {
     effect = "Allow"
-    actions = ["ecs:UpdateService"]
+    actions = [
+      "ecs:UpdateService",
+      "ecs:DescribeServices"
+    ]
     resources = ["arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:service/${var.cluster_name}/*"]
   }
 
