71d1727cfb5cc21f65b98e5278d74ffd4b54623c
Eugeniusz Neugebauer <eugeniusz.neugebauer@telia.no>
2023-05-24 12:25:53 +0200

    Log monitoring values, new default values (#44)
    * Log monitoring values, new default values

--- monitoring-log-volume-alarms/logs-volume-variables.tf
+++ monitoring-log-volume-alarms/logs-volume-variables.tf
@@ -14,3 +14,28 @@ variable "env_description" {
   type        = string
   default     = "()"
 }
+
+
+variable "datapoints_to_alarm" {
+  type = number
+  description = "The number of datapoints that must be breaching to trigger the alarm"
+  default = 10 #after 10 red datapoints in evaluation period (evaluation_periods * metric_query_period = 1h for the current defaults) we should get an alarm
+}
+
+variable "evaluation_periods" {
+  type = number
+  description = "The number of periods over which data is compared to the specified threshold"
+  default = 10
+}
+
+variable "metric_query_period" {
+  type = number
+  description = "Metric query period in sec"
+  default = 360 #every 6 min one datapoint
+}
+
+variable "metric_query_band" {
+  type = number
+  description = "Anomaly detection, based on a standard deviation. Higher number means thicker band, lower number means thinner band."
+  default = 7
+}
