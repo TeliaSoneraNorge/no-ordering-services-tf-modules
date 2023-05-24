12791163cad241cfb7d8244141eaf19c78eda69a
Eugeniusz Neugebauer <eugeniusz.neugebauer@telia.no>
2023-05-24 11:23:20 +0200

    variables

--- monitoring-log-volume-alarms/logs-volume-variables.tf
+++ monitoring-log-volume-alarms/logs-volume-variables.tf
@@ -14,3 +14,28 @@ variable "env_description" {
   type        = string
   default     = "()"
 }
+
+
+variable "datapoints_to_alarm" {
+  type = number
+  description = "The number of datapoints that must be breaching to trigger the alarm"
+  default = 10
+}
+
+variable "evaluation_periods" {
+  type = number
+  description = "The number of periods over which data is compared to the specified threshold"
+  default = 10
+}
+
+variable "metric_query_period" {
+  type = number
+  description = "The number of periods over which data is compared to the specified threshold"
+  default = 900
+}
+
+variable "metric_query_band" {
+  type = number
+  description = "Anomaly detection, based on a standard deviation. Higher number means thicker band, lower number means thinner band."
+  default = 7
+}
