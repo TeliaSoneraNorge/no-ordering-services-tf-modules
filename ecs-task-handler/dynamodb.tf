804e36fe9079fbcab707e2c32bda1c9a759bbea8
Nazar Annagurban <nazar.annagurban@telia.no>
2024-12-05 13:21:20 +0100

    Change capacity to on-demand for ecs-task-handler dynamodb

--- ecs-task-handler/dynamodb.tf
+++ ecs-task-handler/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "ecs_task_failing" {
   name           = var.name
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "Service"
   range_key      = "ExecutionStoppedAt"
 
