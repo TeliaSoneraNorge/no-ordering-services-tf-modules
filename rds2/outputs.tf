c7e0c38943f8380ff06186f10bdea6b1bbb73e7d
Nazar Annagurban <nazar.annagurban@telia.no>
2025-07-29 10:02:58 +0200

    Fix outputs

--- rds2/outputs.tf
+++ rds2/outputs.tf
@@ -2,37 +2,37 @@
 
 output "this_db_instance_arn" {
   description = "The ARN of the RDS instance"
-  value       = module.rds.arn
+  value       = aws_db_instance.rds.arn
 }
 
 output "this_db_instance_endpoint" {
   description = "The connection endpoint"
-  value       = module.rds.endpoint
+  value       = aws_db_instance.rds.endpoint
 }
 
 output "this_db_instance_id" {
   description = "The RDS instance ID"
-  value       = module.rds.id
+  value       = aws_db_instance.rds.id
 }
 
 output "resource_id" {
   description = "The RDS Resource ID of this instance"
-  value       = module.rds.resource_id
+  value       = aws_db_instance.rds.resource_id
 }
 
 output "this_db_instance_name" {
   description = "The database name"
-  value       = module.rds.database_name
+  value       = aws_db_instance.rds.database_name
 }
 
 output "this_db_instance_username" {
   description = "The master username for the database"
-  value       = module.rds.username
+  value       = aws_db_instance.rds.username
 }
 
 output "this_db_instance_port" {
   description = "The database port"
-  value       = module.rds.port
+  value       = aws_db_instance.rds.port
 }
 
 output "this_db_parameter_group_id" {
@@ -47,10 +47,10 @@ output "this_db_parameter_group_arn" {
 
 output "this_db_subnet_group_id" {
   description = "The db subnet group name"
-  value       = module.rds.subnet_group_id
+  value       = aws_db_instance.rds.subnet_group_id
 }
 
 output "this_db_subnet_group_arn" {
   description = "The ARN of the db subnet group"
-  value       = module.rds.subnet_group_arn
+  value       = aws_db_instance.rds.subnet_group_arn
 }
